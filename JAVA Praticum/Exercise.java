68afa2977a5c22f527bbf1b520a500d1160aa36e
SIPHAI <phalsiphai18@gmail.com>
2023-03-04 23:09:26 +0700

    commit

--- JAVA Praticum/Exercise.java
+++ JAVA Praticum/Exercise.java
@@ -5,13 +5,12 @@ public class Exercise {
  public static void main(String[] args) {
   Scanner in = new Scanner(System.in);
    
-  System.out.print("Input first number: ");
+  System.out.print("Input a number: ");
   int num1 = in.nextInt();
    
-  System.out.print("Input second number: ");
-  int num2 = in.nextInt();
-   
-  System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+  for (int i=0; i< 10; i++){
+   System.out.println(num1 + " x " + (i+1) + " = " + 
+     (num1 * (i+1)));
+  }
  }
- 
 }
